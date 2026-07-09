1c0799b54a2c4c8534c3d41e81656791f6dc9f19
Evgeniy Tyurkin <etyurkin@kwarks.org>
2026-07-08 20:05:35 -0400

    Add lisp-sitter 1.1.0 formula

---
+++ Formula/lisp-sitter.rb
@@ -0,0 +1,21 @@
+class LispSitter < Formula
+  desc "Structural editing CLI for Emacs Lisp, Scheme, and Common Lisp"
+  homepage "https://github.com/etyurkin/lisp-sitter"
+  url "https://github.com/etyurkin/lisp-sitter/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "a4e1481eebf523eccb43219d418c8b782e25178c206efd852c92cb9edeec14be"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "crates/lisp-sitter")
+    elisp.install "editor/lisp-sitter.el"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/lisp-sitter --version")
+
+    (testpath/"test.el").write("(defun hello () 42)\n")
+    assert_match "hello", shell_output("#{bin}/lisp-sitter tree #{testpath}/test.el")
+  end
+end
